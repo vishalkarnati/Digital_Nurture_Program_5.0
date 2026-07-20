8c49882a3337c67f19f323602c5f4a8036793128
vishalkarnati <vishalkarnati9@gmail.com>
2026-07-20 20:36:19 +0530

    Add React foiles

---
+++ Week6/SPA-React/student_app/src/Components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class About extends Component{
+    render(){
+        return(
+            <div>
+                <h3>Welcome to About page of Student Management Portal</h3>
+            </div>
+        );
+    }
+}
+
+export default About;
